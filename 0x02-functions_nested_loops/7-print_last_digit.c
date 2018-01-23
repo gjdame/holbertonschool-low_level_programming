2991c78bf2a14308a8b26070f7985458abb6986a
Greg <gregoryjdame@gmail.com>
2018-01-23 21:30:24 +0000

    adjust #7 & #9

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,15 @@ int print_last_digit(int x)
 {
 	int result;
 
-	result = x % 10;
-	if (result < 0)
+	if (x >= 0)
 	{
-		result = -result;
+	result = x % 10;
 	}
+	if (x < 0)
+	{
+		x = -x;
+		result = x % 10;
+	}
+	_putchar(result + '0');
 	return (result);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,16 +8,34 @@ void times_table(void)
 {
 	int x;
 	int y;
+	int z;
+
 	for (x = 0; x <= 9 ; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			x = x * y;
-			_putchar(x / 10 + '0');
-			_putchar(x % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+			z = x * y;
+			if (z <= 9)
+			{
+				_putchar(z + '0');
+				if (!(y >= 9))
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
+			else
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+				if (!(y >= 9))
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
 		_putchar('\n');
 	}
 }
