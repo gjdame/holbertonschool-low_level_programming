4f66920e0b51759113f756b78d248b585d2a9097
Greg <gregoryjdame@gmail.com>
2018-01-23 22:30:49 +0000

    final fix on #9 spaces

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,28 +13,22 @@ void times_table(void)
 	for (x = 1; x <= 9 ; x++)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
 		for (y = 1; y <= 9; y++)
 		{
 			z = x * y;
 				if (z <= 9)
 				{
-					_putchar(z + '0');
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(z + '0');
 				}
 				else
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(z / 10 + '0');
 					_putchar(z % 10 + '0');
-					if (y >= 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
 				}
 		}
 		_putchar('\n');
