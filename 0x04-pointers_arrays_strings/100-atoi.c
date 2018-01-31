8974c0588030b78ba3e93fe820651042ee8c6c71
Greg <gregoryjdame@gmail.com>
2018-01-31 00:19:24 +0000

    style error #10

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -20,33 +20,33 @@ int _atoi(char *s)
 	pos_neg = 0;
 	check_num = 0;
 	sum = 0;
-		for (i = 0; i < _strlen(s); i++)
-		{
-			if ((s[i] > '9' || s[i] < '0') && check_num > 0)
-				break;
-			if (s[i] == '+')
-				pos_neg++;
-			if (s[i] == '-')
-				pos_neg--;
-			if (s[i] >= '0' && s[i] <= '9')
-				check_num++;
-		}
-		i = i - 1;
-		x = 1;
-		while (check_num > 0)
-		{
-			sum = sum + ((s[i] - '0') * x);
-			x = x * 10;
-			i--;
-			check_num--;
-		}
-		if (pos_neg >= 0)
-		{
-			sum = sum * 1;
-		}
-		else if (pos_neg < 0)
-		{
-			sum = sum * -1;
-		}
-		return (sum);
+	for (i = 0; i < _strlen(s); i++)
+	{
+		if ((s[i] > '9' || s[i] < '0') && check_num > 0)
+			break;
+		if (s[i] == '+')
+			pos_neg++;
+		if (s[i] == '-')
+			pos_neg--;
+		if (s[i] >= '0' && s[i] <= '9')
+			check_num++;
 	}
+	i = i - 1;
+	x = 1;
+	while (check_num > 0)
+	{
+		sum = sum + ((s[i] - '0') * x);
+		x = x * 10;
+		i--;
+		check_num--;
+	}
+	if (pos_neg >= 0)
+	{
+		sum = sum * 1;
+	}
+	else if (pos_neg < 0)
+	{
+		sum = sum * -1;
+	}
+	return (sum);
+}
