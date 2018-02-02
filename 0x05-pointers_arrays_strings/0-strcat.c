30b77760903e8e98fcb30315b144591745fc7015
Greg <gregoryjdame@gmail.com>
2018-02-02 21:19:04 +0000

    cleaning up #0

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -14,21 +14,19 @@ char *_strcat(char *dest, char *src)
 	int len_src;
 	int i;
 
-	len_src = 0;
 	len_dest = 0;
 	while (dest[len_dest] != '\0')
 	{
 		len_dest++;
 	}
-	while (src[len_src] != '\0')
-	{
-		len_src++;
-	}
 	i = 0;
-	while (i < len_src + 1)
+	len_src = 0;
+	while (src[len_src] != '\0')
 	{
 		dest[len_dest + i] = src[i];
 		i++;
+		len_src++;
 	}
+	dest[len_dest + i] = '\0';
 	return (dest);
 }
