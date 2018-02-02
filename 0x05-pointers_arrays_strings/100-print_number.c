647762de2fc9b73589fecfc203f064af7d1755f4
Greg <gregoryjdame@gmail.com>
2018-02-02 02:33:56 +0000

    advanced #1 0x05

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ * print_number - print a number
+ * @n:int
+ * print_number: print a number
+ *
+ * Return: int
+ */
+void print_number(int n)
+{
+	unsigned int x;
+	unsigned int y;
+	int i;
+	int counter;
+	int z;
+
+	z = 0;
+	i = 1;
+	x = n;
+	y = n;
+	counter = 0;
+
+	if (n == 0)
+		_putchar ('0');
+	if (n < 0)
+	{
+		_putchar ('-');
+		x = x * -1;
+		y = y * -1;
+	}
+
+	while (y > 0)
+	{
+		y = y / 10;
+		counter++;
+	}
+
+	for (z = 1; z < counter; z++)
+	{
+		i = i * 10;
+	}
+	for (z = 0; z < counter; z++)
+	{
+		_putchar(x / i + '0');
+		x = x % i;
+		i = i / 10;
+	}
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -10,4 +10,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 int _putchar(char c);
+void print_number(int n);
 #endif
