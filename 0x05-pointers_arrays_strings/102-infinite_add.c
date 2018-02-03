8993679763903e64cd2bc5ba795baedddea99d9f
Greg <gregoryjdame@gmail.com>
2018-02-03 01:02:28 +0000

    #10 advanced

---
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * infinite_add - add two numbers
+ * @n1: number
+ * @n2: number
+ * @r: result
+ * @size_r: buffer size
+ *
+ * infinite_add - adds two numbers
+ *
+ * Return: @r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int c;
+
+	r = n1 + n2;
+	c = 0;
+	while(s[r] != '\0')
+		c++;
+	if ( > size_r)
+		return (0);
+	return(r);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -11,4 +11,5 @@ char *leet(char *);
 char *rot13(char *);
 int _putchar(char c);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
