0f5059dcb3f493eaaa649334dd83046058cf6c03
Greg <gregoryjdame@gmail.com>
2018-02-06 02:58:58 +0000

    #5

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -9,23 +9,23 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+
 	int i;
 	int j;
-
 	if (needle == '\0')
-	{
-		return (0);
-	}
+		return(haystack);
 	i = 0;
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[0])
 		{
 			j = 0;
-			while (needle[j] == haystack[i + j] && needle[j] != '\0')
+			while (haystack[i + j] == needle[j] && needle[j] != '\0')
 			{
 				j++;
 			}
+			if (needle[j] == '\0')
+				break;
 		}
 		i++;
 	}
