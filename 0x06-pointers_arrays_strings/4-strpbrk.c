9cf7643b652599d72b6ae755bf8a3100fc8202a2
Greg <gregoryjdame@gmail.com>
2018-02-06 02:05:46 +0000

    fix 4 finally

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -12,9 +12,9 @@ char *_strpbrk(char *s, char *accept)
 	int i;
 	int j;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
