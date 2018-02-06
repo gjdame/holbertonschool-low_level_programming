f6a24a38a90ea090aba863e918bc7457953545b8
Greg <gregoryjdame@gmail.com>
2018-02-06 05:22:51 +0000

    fix number 5 again

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -20,10 +20,12 @@ char *_strstr(char *haystack, char *needle)
 	{
 		if (haystack[i] == needle[0])
 		{
-			for (j = 0; needle[j] != '\0'; j++)
+			for (j = 0; needle[j] != '\0' && haystack[i + j] != '\0'; j++)
 			{
 				if (haystack[i + j] != needle[j])
 					break;
+				if (haystack[i + j] == '\0')
+					return (0);
 			}
 			if (needle[j] == '\0')
 				return (haystack + i);
