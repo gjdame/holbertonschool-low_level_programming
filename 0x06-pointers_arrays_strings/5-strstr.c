f74b4b3f8f3db25d185706329fe03d1822c501ca
Greg <gregoryjdame@gmail.com>
2018-02-06 07:05:23 +0000

    last try at 5 for today

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,35 +1,32 @@
 #include "holberton.h"
-
+#define NULL 0
 /**
- * _strstr - finds 1st occurence of substring
- * @haystack: string to be searched
- * @needle: string to find in haystack
- * _strstr: 1st occurents of substring
  *
- * Return: beginning of substring
  */
 char *_strstr(char *haystack, char *needle)
 {
-
 	int i;
 	int j;
 
-	if (needle == 0)
+
+	if (needle[0] == NULL)
 		return (haystack);
-	for (i = 0; haystack[i] != '\0'; i++)
+	i = 0;
+	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[0])
 		{
-			for (j = 0; needle[j] != '\0' && haystack[i + j] != '\0'; j++)
+			j = 0;
+			while (needle[j] != '\0')
 			{
 				if (haystack[i + j] != needle[j])
 					break;
-				if (haystack[i + j] == '\0')
-					return (0);
+				j++;
 			}
 			if (needle[j] == '\0')
 				return (haystack + i);
 		}
+		i++;
 	}
 	return (0);
 }
