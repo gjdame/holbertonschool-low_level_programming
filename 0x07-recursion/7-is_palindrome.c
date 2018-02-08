94ce8c293888733de770058ba54282c500161333
Greg <gregoryjdame@gmail.com>
2018-02-08 02:47:57 +0000

    #7

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,36 +1,49 @@
 #include "holberton.h"
 #define NULL 0
 /**
+ * _strlen_recursion - checks stringlength
+ * @s: string to be checked
+ *
+ * Return: @i for length
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 1;
+	if (*s)
+	{
+		i = i + _strlen_recursion(s + 1);
+		return (i++);
+	}
+	return (0);
+}
+/**
+ * palindrome - checks if palindrome
+ * @s: char string
+ * @y: length of a end of string
+ * Return: if string is palindrome (1) or not (0)
+ */
+int palindrome(char *s, int y)
+{
+	if ((*s) != *(s + y))
+		return (0);
+	if (*(s + 1) == '\0')
+		return (1);
+	return (palindrome(s + 1, y - 2));
+}
+/**
  * is_palindrome - checks if input is a palindrome
  * @s: string to be checked
  *
  * Return: 1 if true, 0 if false
  */
-int _strlen_recursion(char *s)
-{
-        int i;
-
-        i = 1;
-        if (*s)
-        {
-                i = i + _strlen_recursion(s + 1);
-                return (i++);
-        }
-        return (0);
-}
-int palindrome(char *s, int x, int y)
-{
-	if((s + x) != (s + y))
-		return (0);
-	if((s + x) == (s + y) && y / 2 < y)
-		return (palindrome(s, x + 1, y - 1));
-	return (1);
-}
 int is_palindrome(char *s)
 {
 	int y;
+
+	y = _strlen_recursion(s);
 	if (*s == NULL)
 		return (0);
-	y = _strlen_recursion(s);
-	return (palindrome(s, 0 , y));
+	return (palindrome(s, y - 1));
 }
