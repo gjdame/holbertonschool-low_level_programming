96bbde8734d9bc40d7b48e32dcec46f4a8e29316
Greg <gregoryjdame@gmail.com>
2018-02-08 23:01:12 +0000

    advanced 0x07 initial commit

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #define NULL 0
 /**
- * wildcmp
+ * wildcmp - compares 2 strings and returns if they can be close to identitical
  * @s1: string 1
  * @s2: string 2
  * Return: 1 if true, 0 if false
@@ -10,9 +10,9 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == NULL && *s2 == NULL)
 		return (1);
-	if (*s1 == *s2 )
+	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1)) || (wildcmp(s1 + 1, s2));
+		return ((wildcmp(s1, s2 + 1)) || (wildcmp(s1 + 1, s2)));
 	return (0);
 }
