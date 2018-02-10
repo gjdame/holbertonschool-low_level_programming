5114ea1e629d960f4c81e86611f8a450bf665226
Greg <gregoryjdame@gmail.com>
2018-02-10 20:38:54 +0000

    fix 3-mul for error message

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -12,11 +12,14 @@ int main(int argc, char *argv[])
 	int i;
 	int j;
 
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
 
-	if (argc < 2)
-		printf("Error");
+	i = atoi(argv[1]);
+        j = atoi(argv[2]);
 
 	printf("%d\n", i * j);
 	return (0);
