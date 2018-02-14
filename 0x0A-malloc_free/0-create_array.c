857bb988a1acd0d101b50c79c496d557df45e746
Greg <gregoryjdame@gmail.com>
2018-02-14 01:40:00 +0000

    fix #0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -17,10 +17,8 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	s = malloc((size + 1) * sizeof(char));
-	if (s == 0)
-		return (0);
 
-	if (s == NULL)
+	if (s == 0)
 		return (NULL);
 
 	i = 0;
