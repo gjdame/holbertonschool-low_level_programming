4fc3b895700be26bfa04033e04c61d01f475c211
Greg <gregoryjdame@gmail.com>
2018-02-15 21:38:07 +0000

    #2 fix

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	s = malloc(size * nmemb);
+	s = malloc(sizeof(size) * nmemb);
 	if (s == NULL)
 		return (NULL);
 	i = 0;
