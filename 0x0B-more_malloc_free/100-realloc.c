803569b9f4f99cebd7605f9dd90f7abae074c231
Greg <gregoryjdame@gmail.com>
2018-02-16 06:45:34 +0000

    #4 test 2

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size > old_size)
 	{
-		s = malloc(old_size);
+		s = malloc(new_size);
 		free(ptr);
 		return (s);
 	}
@@ -29,8 +29,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size == 0 && ptr != NULL)
 		free(ptr);
-	s = malloc(new_size);
-	s = ptr;
-	free(ptr);
-	return (s);
+	return(ptr);
 }
