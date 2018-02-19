ef31347322bc53692763facf2c90e1fefdfac4ec
gjdame <gregoryjdame@gmail.com>
2018-02-19 13:18:38 -0800

    style

--- 0x0C-preprocessor/1-pi.h
+++ 0x0C-preprocessor/1-pi.h
@@ -1,3 +1,3 @@
 #ifndef PI
-#define PI 3.1459265359
+#define PI 3.14159265359
 #endif
--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * 
- *
+ * main - print file name
+ * Return: 0
  */
-int  main(void)
+int main(void)
 {
-  printf("%s\n", __FILE__ );
-  return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
