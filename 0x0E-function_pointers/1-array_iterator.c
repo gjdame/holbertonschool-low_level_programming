8fdf4058a190bc37252d7734b456e2ca1c24f931
gjdame <gregoryjdame@gmail.com>
2018-02-21 15:57:57 -0800

    null

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 unsigned int i;
 
 if (array == NULL || size <= 0 || action == NULL)
-return (0);
+return;
 
 i = 0;
 while (i < size)
