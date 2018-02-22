c28291e6b057051979ddf00ce7d420ade5e2297d
gjdame <gregoryjdame@gmail.com>
2018-02-22 10:17:31 -0800

    opcodes. Still unsure of what an opcode is.

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - returns op codes for address of main
+* @argc: num of arguments
+* @argv: char array
+* Return: print opcodes
+*/
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+char *s = (char *) &main;
+int num = atoi(argv[1]);
+int x;
+
+if (num < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+
+x = 0;
+while (x < num)
+{
+printf("%.02hhx ", s[x]);
+x++;
+}
+printf("\n");
+return (0);
+}
