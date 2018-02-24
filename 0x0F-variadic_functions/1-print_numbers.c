a69cc4c47c55e7b634d63ddbe8835877467e2b47
gjdame <gregoryjdame@gmail.com>
2018-02-24 13:43:23 -0800

    fix for NULL #1 & betty

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -2,25 +2,30 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
-*
+* print_numbers - print numbers
+* @separator: separator
+* @n: unsigned int
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-  va_list list;
-  unsigned int i;
-  const char *s = separator;
+va_list list;
+unsigned int i;
+const char *s = separator;
 
-  va_start(list, n);
 
-  i = 0;
-  while (i < n)
-  {
-    printf("%d",va_arg(list, int));
-    if (i < n - 1)
-    printf("%s", s);
-    i++;
-  }
-  printf("\n");
+va_start(list, n);
 
-  va_end(list);
+i = 0;
+while (i < n)
+{
+printf("%d", va_arg(list, int));
+if (i < n - 1 && s != NULL)
+printf("%s", s);
+
+printf("");
+i++;
+}
+printf("\n");
+
+va_end(list);
 }
