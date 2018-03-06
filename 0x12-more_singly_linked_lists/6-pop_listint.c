406344dc464574914dad6df676f1f36f7e77e134
gjdame <gregoryjdame@gmail.com>
2018-03-06 08:49:36 -0800

    fix 6

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -7,9 +7,20 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
+	int i = 0;
+
+	if (*head == NULL)
+		return (0);
 
 	tmp = *head;
+	i = tmp->n;
 	tmp = tmp->next;
+	if (tmp == NULL)
+	{
+		free (tmp);
+		return (0);
+	}
+	*head = tmp;
 
-	return (tmp->n);
+	return (i);
 }
