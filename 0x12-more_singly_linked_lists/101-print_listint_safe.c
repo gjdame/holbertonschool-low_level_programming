d11c7f2f80a67c2eaf059639a80b5fae282d9f67
gjdame <gregoryjdame@gmail.com>
2018-03-06 14:21:29 -0800

    initial commit 101

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints all elements of listint
+ * @h: pointer to first node of list
+ * Return: num of elements
+ */
+size_t print_listint_safe(const listint_t *h)
+{
+	int i;
+	int j;
+	const listint_t *tmp;
+	const listint_t *current;
+
+	if (h == NULL)
+		exit (98);
+
+	tmp = h;
+
+	i = 0;
+	while (tmp != NULL)
+	{
+		current = h;
+		j = 0;
+		while (j < i)
+		{
+			if (current == tmp)
+			{
+				printf("-> [%p] %d\n",(void *)  &tmp, tmp->n);
+				return (i);
+			}
+			current = current->next;
+			j++;
+		}
+		printf("[%p] %d\n",(void *) &tmp->next,  tmp->n);
+		tmp = tmp->next;
+		i++;
+	}
+	return (0);
+}
