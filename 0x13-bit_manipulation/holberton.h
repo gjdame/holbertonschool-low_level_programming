c9872608d56e2aa1246e49d2e992909dc9dee8d2
gjdame <gregoryjdame@gmail.com>
2018-03-08 15:54:04 -0800

    fix holberton.h

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,8 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
