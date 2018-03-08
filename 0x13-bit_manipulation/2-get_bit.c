480309059ad6d1f531ca80467fc155bbfc54ed5c
gjdame <gregoryjdame@gmail.com>
2018-03-08 14:45:05 -0800

    add return -1

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -5,5 +5,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 32)
+		return (-1);
 	return ((n >> index) & 1);
 }
--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,9 +1,16 @@
 #include "holberton.h"
+#include "2-get_bit.c"
 /**
  *
  *
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	return ((*n) |= (1 << index));
+	if (index > 32)
+		return (-1);
+
+	(*n) |= (1 << index);
+	if (get_bit((*n), index) == 1)
+		return (1);
+	return (-1);
 }
