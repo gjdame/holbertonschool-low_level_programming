f198b7f15a98448a8495a9183f0fb18feb97e5e0
gjdame <gregoryjdame@gmail.com>
2018-03-09 09:40:41 -0800

    recursive way for #1

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -6,34 +6,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
 
-	mask <<= ((sizeof(mask) * 8) - 1);
-
-	if (n == 1)
-	{
-		_putchar('1');
-		return;
-	}
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	while (mask > 0)
-	{
-		if ((n & mask) == 0)
-			mask = mask >> 1;
-		else
-			break;
-	}
-	while (mask > 0)
-	{
-		if ((n & mask) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-		mask = mask >> 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar ((n & 1) + '0');
 }
