44492ba2b52e873a070012be4224f0090a1bb777
gjdame <gregoryjdame@gmail.com>
2018-03-13 15:15:31 -0700

    1 again

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -18,12 +18,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 		if (fd == -1)
 			return (-1);
+
+		if (text_content == NULL)
+			text_content = " ";
 
 	writen = write(fd, text_content, len);
 	if (writen == -1)
