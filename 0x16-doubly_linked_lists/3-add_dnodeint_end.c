1fc41584a0daaaf6918088a57520e9b9b20edde0
gjdame <gregoryjdame@gmail.com>
2018-04-14 20:25:54 -0700

    betty style #3

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		new_node->next = NULL;
 		new_node->prev = NULL;
 		*head = new_node;
-		return(new_node);
+		return (new_node);
 	}
 
 	while (tmp->next != NULL)
