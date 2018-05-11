0a4a10a12f9bf1e999f894d8b2a5e53bdf04b10b
gjdame <gregoryjdame@gmail.com>
2018-05-11 18:22:13 +0000

    add null checks for 4

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -10,6 +10,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned int index;
 	hash_node_t *check;
 
+	if (ht == NULL || strlen(key) == 0)
+		return (NULL);
+
 	index = key_index((const unsigned char *) key, ht->size);
 	check = ht->array[index];
 	if (check == NULL)
