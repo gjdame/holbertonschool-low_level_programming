237f50ee0ad83739903a6299a89c3c072bb2dfbe
gjdame <gregoryjdame@gmail.com>
2018-05-29 21:35:38 +0000

    rework of heap sort

--- 0x1A-sorting_algorithms/104-heap_sort.c
+++ 0x1A-sorting_algorithms/104-heap_sort.c
@@ -4,51 +4,45 @@
  *
  *
  */
-void sift_down(int *array, int start, int end, size_t size)
+void heapify(int *array, int min, int max, size_t size)
 {
-        int root = start;
-        int child;
-        int tmp;
-        while((root * 2) + 1 <= end)
-        {
-                child = root * 2 + 1;
-                if (child + 1 <= end && array[child] < array[child + 1])
-                {
-                        child++;
-                }
-                if (array[root] < array[child])
-                {
-                        tmp = array[root];
-                        array[root] = array[child];
-                        array[child] = tmp;
-			print_array(array, size);
-		}
-                else
-                        return;
-        }
-}
-void heapify(int *array, size_t size)
-{
-	int start = (size - 2) / 2;
-	while (start >= 0)
+	int root = min;
+	int left = 2 * root + 1;
+	int right = 2 * root + 2;
+	int tmp;
+
+	if (left < max && array[left] > array[root])
+		root = left;
+
+	if (right < max && array[right] > array[root])
+		root = right;
+
+	if (min != root)
 	{
-		sift_down(array, start, size - 1, size);
-		start--;
+		tmp = array[root];
+		array[root] = array[min];
+		array[min] = tmp;
+		print_array(array, size);
+		heapify(array, root, max, size);
 	}
 }
+
 void heap_sort(int *array, size_t size)
 {
-	unsigned int end  = size - 1;
-	unsigned int tmp;
-	heapify(array, size);
+	int i;
+	int tmp;
 
-	while (end > 0)
+	for (i = size/2 - 1; i >= 0; i--)
+		heapify(array, i, size, size);
+
+	i = size - 1;
+	while (i > 0)
 	{
-		tmp = array[end];
-		array[end] = array[0];
+		tmp = array[i];
+		array[i] = array[0];
 		array[0] = tmp;
 		print_array(array, size);
-		end--;
-		sift_down(array, 0, end, size);
+		heapify(array, 0, i, size);
+		i--;
 	}
 }
