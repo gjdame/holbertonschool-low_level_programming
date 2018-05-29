4b719ec083475091f65e400e6a8811e342b7d0e2
gjdame <gregoryjdame@gmail.com>
2018-05-29 18:04:18 +0000

    update header

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -1,7 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,11 +18,15 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-void print_list(const listint_t *list);
+
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+
 #endif
