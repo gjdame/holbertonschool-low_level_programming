189815f486729c467d346a0f76126b152db3f6ac
gjdame <gregoryjdame@gmail.com>
2018-05-29 20:55:59 +0000

    sort.h update

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -28,5 +28,5 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
-
+void heap_sort(int *array, size_t size);
 #endif
