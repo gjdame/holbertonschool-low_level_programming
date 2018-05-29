bd19b26aff7735d10f87d939f0679b143f6ed8cd
gjdame <gregoryjdame@gmail.com>
2018-05-29 23:48:44 +0000

    add quick sort hoare

---
+++ 0x1A-sorting_algorithms/107-quick_sort_hoare.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+
+/**
+ * partition - this partitions the array and swaps
+ * @array: input array
+ * @low: input low index
+ * @high: input high index
+ * @size: input size of array
+ * Return: return the partioned index
+ */
+int partition(int *array, size_t low, size_t high, size_t size)
+{
+	int pivot = array[low];
+	int x = low - 1;
+	int y = high + 1;
+	int tmp;
+
+	while(1)
+	{
+		do
+		{
+			x++;
+		}while(array[x] < pivot);
+
+		do
+		{
+			y--;
+		}while(array[y] > pivot);
+
+		if (x >= y)
+			return (y);
+		tmp = array[x];
+		array[x] = array[y];
+		array[y] = tmp;
+		print_array(array, size);
+	}
+}
+void quicksort(int *array, int low, int high, size_t size)
+{
+
+	int p;
+
+	if (low < high)
+	{
+		p = partition(array, low, high, size);
+		quicksort(array, low, p, size);
+		quicksort(array, p + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sory array using quick sort method
+ * @array: input array
+ * @size: input size of array
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	unsigned int low = 0;
+        unsigned int high = size - 1;
+
+	if (size == 0 || size == 1)
+		return;
+
+	quicksort(array, low, high, size);
+}
--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -29,4 +29,5 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
