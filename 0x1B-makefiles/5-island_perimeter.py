e6ada67da3b2c6c826b78f9591974ecf32a9c6c1
gjdame <gregoryjdame@gmail.com>
2018-06-15 22:13:30 +0000

    initial add #5

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    count = 0
+    touches = 0
+    height = 0
+    width = 0
+    if grid:
+        for row in grid:
+            height += 1
+        width = len(grid[0])
+        for x in range(height):
+            for y in range(width):
+                if grid[x][y] == 1:
+                    count += 1
+                    if y - 1 >= 0:
+                        if grid[x][y - 1] == 1:
+                            touches += 1
+                    if y + 1 >= 0:
+                        if grid[x][y + 1] == 1:
+                            touches += 1
+                    if x + 1 <= height:
+                        if grid[x + 1][y] == 1:
+                            touches += 1
+                    if x - 1 >= 0:
+                        if grid[x - 1][y] == 1:
+                            touches += 1
+        count = count * 4 - touches
+        return(count)
