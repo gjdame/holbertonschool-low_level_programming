30be43a38cea5e71754e13faa6ea1000d36fd1ca
gjdame <gregoryjdame@gmail.com>
2018-07-03 22:04:51 +0000

    add 100 initial commit

---
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -0,0 +1,68 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - depth of node in a tree
+ * @node: node to check
+ * Return: size_t depth
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+        size_t depth;
+
+        if (node == NULL)
+                return(0);
+
+        if (node->parent == NULL)
+                return(0);
+        depth = binary_tree_depth(node->parent);
+        depth++;
+        return (depth);
+}
+/**
+ *
+ *
+ *
+ *
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	size_t first_depth;
+	size_t second_depth;
+
+	if (first->parent == second)
+		return (first->parent);
+	if (second->parent == first)
+		return (second->parent);
+
+	if (first->parent == NULL)
+		return (NULL);
+
+	if (second->parent == NULL)
+		return (NULL);
+
+	first_depth = binary_tree_depth(first);
+	second_depth = binary_tree_depth(second);
+
+	if (first_depth > second_depth)
+	{
+		while (first_depth > second_depth)
+		{
+			first = first->parent;
+			first_depth--;
+		}
+	}
+	if (second_depth > first_depth)
+	{
+		while (second_depth > first_depth)
+		{
+			second = second->parent;
+			second_depth--;
+		}
+	}
+	while (first->parent != NULL && second->parent != NULL)
+	{
+		if (first->parent == second->parent)
+			return (first->parent);
+	}
+
+	return (NULL);
+}
--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -21,6 +21,9 @@ typedef struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -42,5 +45,16 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
 
 #endif /* _BINARY_TREES_H_ */
