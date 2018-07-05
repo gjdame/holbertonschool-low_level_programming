4b3ecc90377b5777c89dfbd6e9bfdb54ea330996
gjdame <gregoryjdame@gmail.com>
2018-07-05 17:46:32 +0000

    fix #5

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -8,12 +8,9 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left || node->right)
-	{
-		if (node->parent == NULL)
-			return (1);
-		else
-			return (0);
-	}
+
+	if (node->parent == NULL)
+		return (1);
+
 	return (0);
 }
