641d57fe7249ea6a8e31ec9fa18591b1bdc6e696
gjdame <gregoryjdame@gmail.com>
2018-07-05 21:29:42 +0000

    add 102 the math way incrementing index

--- 0x1C-binary_trees/102-binary_tree_is_complete.c
+++ 0x1C-binary_trees/102-binary_tree_is_complete.c
@@ -1,5 +1,40 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_nodes - how many nodes on a tree
+ * @tree: root node of tree
+ * Return: number of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+	size_t nodes_right, nodes_left;
+
+	if (tree == NULL)
+		return (0);
+
+	nodes_left = binary_tree_nodes(tree->left) + 1;
+	nodes_right = binary_tree_nodes(tree->right) + 1;
+	return (nodes_right + nodes_left - 1);
+
+}
+/**
+ *
+ *
+ *
+ *
+ */
+int is_complete(const binary_tree_t *tree, int index, int nodes)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (index >= nodes)
+                return (0);
+
+	return (is_complete(tree->left, 2 * index + 1, nodes) &&
+		is_complete(tree->right, 2 * index + 2, nodes));
+}
+/**
  * binary_tree_is_complete - is binary tree complete (all to left)
  * @tree: root node
  * Return: 1 if true 0 if false
@@ -7,29 +42,12 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	int right, left;
+	int index = 0;
+	size_t nodes = binary_tree_nodes(tree);
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+	return (is_complete(tree, index, (int)nodes));
 
-	if (tree->right)
-		if ((tree->right->left) && !(tree->right->right))
-			return (0);
-
-	if (tree->left == NULL && tree->right != NULL)
-		return (0);
-
-	if (tree->left != NULL && tree->right == NULL)
-		return (1);
-
-	right = binary_tree_is_complete(tree->right);
-	left = binary_tree_is_complete(tree->left);
-
-	if (right == 0 || left == 0)
-		return (0);
-
-	return (1);
 }
