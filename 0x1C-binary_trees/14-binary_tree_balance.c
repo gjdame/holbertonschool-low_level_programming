7db5a6974625c76e4ab10a36b8b710c67ef2a6fa
gjdame <gregoryjdame@gmail.com>
2018-07-05 17:52:22 +0000

    re work of 14 to use height function

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,30 +1,48 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - height binary tree
+ * @tree: root node of tree
+ * Return: height of tree
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left;
+	size_t height_right;
+
+
+	if (tree == NULL)
+		return (0);
+
+	if (!(tree->left) && !(tree->right))
+		return (0);
+	height_left = binary_tree_height(tree->left);
+	height_left++;
+	height_right = binary_tree_height(tree->right);
+	height_right++;
+	if (height_left > height_right)
+		return (height_left);
+	else
+		return (height_right);
+}
+
+/**
 * binary_tree_balance - checks if tree is balanced
 * @tree: root node of tree
 * Return: balance of tree
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	const binary_tree_t *root = tree;
 	int pos = 0;
 	int neg = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->right)
-		while (tree->right)
-		{
-			neg++;
-			tree = tree->right;
-		}
-	if (root->left)
-		while (root->left)
-		{
-			pos++;
-			root = root->left;
-		}
+	neg = binary_tree_height(tree->right);
+	neg = neg * -1;
+
+	pos = binary_tree_height(tree->left);
 
 	return (pos - neg);
 }
