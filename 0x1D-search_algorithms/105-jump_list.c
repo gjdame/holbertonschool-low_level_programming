b6a01b9fa4818ec1ad6c82bf365840edf3ed3761
gjdame <gregoryjdame@gmail.com>
2018-08-07 22:12:49 +0000

    add 105

---
+++ 0x1D-search_algorithms/105-jump_list.c
@@ -0,0 +1,60 @@
+#include "search_algos.h"
+#include <math.h>
+/**
+ *
+ *
+ *
+ */
+listint_t *move(listint_t *list, int block)
+{
+        int i = 0;
+        while(i < block)
+        {
+                list = list->next;
+                if (list->next == NULL)
+                        return(list);
+                i++;
+        }
+	return(list);
+}
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	int block;
+
+	listint_t *runner = list;
+	listint_t *prev;
+
+	if (list == NULL)
+		return(list);
+
+	block = sqrt((int)size);
+
+
+	while(runner->n < value)
+	{
+		prev = runner;
+		runner = move(runner, block);
+		if (runner->next == NULL)
+			break;
+		printf("Value checked at index [%lu] = [%d]\n", runner->index, runner->n);
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", prev->index, runner->index);
+	while(prev->index < runner->index)
+	{
+		if (prev->n == value)
+		{
+			printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+
+			return(prev);
+		}
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		prev = prev->next;
+		if (prev->next == NULL)
+		{
+			printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+			return(NULL);
+		}
+	}
+	return(NULL);
+}
