4477d887d507f2ee463df4b2ffab3f6e582a0941
gjdame <gregoryjdame@gmail.com>
2018-08-08 21:46:33 +0000

    add upated header

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -4,6 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+/**
  * struct listint_s - singly linked list
  *
  * @n: Integer
@@ -30,5 +48,8 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
 listint_t *create_list(int *array, size_t size);
 void free_list(listint_t *list);
 void print_list(const listint_t *list);
-
+void free_skiplist(skiplist_t *list);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
