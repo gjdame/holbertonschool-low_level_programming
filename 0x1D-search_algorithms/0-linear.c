967aeb9e1f98ca7f4f92633405e3c9565e935048
gjdame <gregoryjdame@gmail.com>
2018-08-08 16:36:25 +0000

    betty style 0

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -1,7 +1,10 @@
 #include "search_algos.h"
 /**
- * linear_search: linear search algorithm
- * linear search algorithm
+ * linear_search - linear search algorithm
+ * @array: array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: position or -1 if not found
  *
  */
 int linear_search(int *array, size_t size, int value)
@@ -9,18 +12,18 @@ int linear_search(int *array, size_t size, int value)
 	int i = 0;
 
 	if (array == NULL)
-		return(-1);
+		return (-1);
 
-	while(i < (int)size)
+	while (i < (int)size)
 	{
 		if (array[i] == value)
 		{
 			printf("Value checked array[%d] = [%d]\n", i, array[i]);
-			return(i);
+			return (i);
 		}
 		else
 			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		i++;
 	}
-	return(-1);
+	return (-1);
 }
