89bc80dfc68b3ea6c7050b7667dd3e68f92de36c
gjdame <gregoryjdame@gmail.com>
2018-08-08 20:46:17 +0000

    add 100 updates for edge cases

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -9,37 +9,30 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int block;
-	int i;
+	int block, block_size;
+	int i = 0;
 	int holder = 0;
 
 	if (array == NULL)
 		return (-1);
 
 	block = sqrt((int)size);
-	i = block;
+	block_size = block;
 	printf("Value checked array[%d] = [%d]\n", holder, array[holder]);
-	while (array[block] <= value)
+	while (array[block] <= value && block <= (int)size)
 	{
-		if (array[holder] == value)
-			return (holder);
-		if (array[block] == value)
+		if (array[i] == value || array[block] == value)
 			break;
 		holder = block;
-		if (holder < (int)size)
-			printf("Value checked array[%d] = [%d]\n", holder, array[holder]);
-		block = block + i;
+		printf("Value checked array[%d] = [%d]\n", holder, array[holder]);
+		block = block + block_size;
 	}
-	if (holder > (int)size)
-	{
-		holder -= i;
-		block -= i;
-	}
+
 	printf("Value found between indexes [%d] and [%d]\n", holder, block);
 	while (holder <= block)
 	{
 
-		if (holder > (int)size - 1)
+		if (holder >= (int)size)
 			return (-1);
 		printf("Value checked array[%d] = [%d]\n", holder, array[holder]);
 		if (array[holder] == value)
