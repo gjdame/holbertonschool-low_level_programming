b87d298fecbe78e4ed7d3f48abeb76f8db70f548
gjdame <gregoryjdame@gmail.com>
2018-08-08 17:30:21 +0000

    bettty 102

--- 0x1D-search_algorithms/102-interpolation.c
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -1,42 +1,50 @@
 #include "search_algos.h"
 /**
- *
- *
- *
- *
+ * interpolation_search - interpolation search algo
+ * @array: sorted array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: pos or -1
  */
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0;
 	size_t high  = size - 1;
-	size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+	size_t pos = low + (((double)(high - low) / (array[high] - array[low]))
+			    * (value - array[low]));
 
 	if (array == NULL)
-		return(-1);
+		return (-1);
 
-	while(high != low)
+	while (high != low)
 	{
 		if (pos > size)
 		{
-			printf("Value checked array[%d] is out of range\n", (int)pos);
-			return(-1);
+			printf("Value checked array[%d] is out of range\n",
+			       (int)pos);
+			return (-1);
 		}
-		printf("Value checked array[%d] = [%d]\n", (int)pos, array[(int)pos]);
+		printf("Value checked array[%d] = [%d]\n", (int)pos,
+		       array[(int)pos]);
 		if (array[pos] == value)
-			return(pos);
+			return (pos);
 		if (array[pos] > value)
 		{
 			high--;
-			pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+			pos = low + (((double)(high - low) /
+				      (array[high] - array[low]))
+				     * (value - array[low]));
 		}
 		if (array[pos] < value)
 		{
 			low++;
-			pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+			pos = low + (((double)(high - low) /
+				      (array[high] - array[low])) *
+				     (value - array[low]));
 		}
 	}
 	if (array[low] == value)
-		return(low);
+		return (low);
 
-	return(-1);
+	return (-1);
 }
