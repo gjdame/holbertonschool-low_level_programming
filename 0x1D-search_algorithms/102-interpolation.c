7f2a7aebc424a18e1a746929657e933d6eb7bf99
gjdame <gregoryjdame@gmail.com>
2018-08-08 21:03:01 +0000

    add 102 updates

--- 0x1D-search_algorithms/102-interpolation.c
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -18,7 +18,7 @@ int interpolation_search(int *array, size_t size, int value)
 	pos = low + (((double)(high - low) / (array[high] - array[low]))
 			    * (value - array[low]));
 
-	while (high != low)
+	while (low <= high)
 	{
 		if (pos > size)
 		{
