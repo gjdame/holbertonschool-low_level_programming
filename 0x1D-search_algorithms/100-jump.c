3bfe79df29f01e9a66036a2a9dce4d7e0db775b3
gjdame <gregoryjdame@gmail.com>
2018-08-08 17:01:26 +0000

    add jump betty style

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -1,9 +1,11 @@
 #include "search_algos.h"
 #include <math.h>
 /**
- *
- *
- *
+ * jump_search - jump search algorithm
+ * @array: sorted array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: position or -1
  */
 int jump_search(int *array, size_t size, int value)
 {
@@ -12,15 +14,15 @@ int jump_search(int *array, size_t size, int value)
 	int holder = 0;
 
 	if (array == NULL)
-		return(-1);
+		return (-1);
 
 	block = sqrt((int)size);
 	i = block;
 	printf("Value checked array[%d] = [%d]\n", holder, array[holder]);
-	while(array[block] <= value)
+	while (array[block] <= value)
 	{
 		if (array[holder] == value)
-			return(holder);
+			return (holder);
 		if (array[block] == value)
 			break;
 		holder = block;
@@ -34,15 +36,15 @@ int jump_search(int *array, size_t size, int value)
 		block -= i;
 	}
 	printf("Value found between indexes [%d] and [%d]\n", holder, block);
-	while(holder <= block)
+	while (holder <= block)
 	{
 
 		if (holder > (int)size - 1)
-			return(-1);
+			return (-1);
 		printf("Value checked array[%d] = [%d]\n", holder, array[holder]);
 		if (array[holder] == value)
-			return(holder);
+			return (holder);
 		holder++;
 	}
-	return(-1);
+	return (-1);
 }
